076ca062e1b091e5ea85884d37fbd5abc529522b
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-13 17:21:25 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,6 @@ def position_taken?(board, token)
   if board[token] == " " || board[token] == "" || board[token] == nil
     false
   elsif board[token] =="X" || board[token] == "O"
+    true
   end
 end
