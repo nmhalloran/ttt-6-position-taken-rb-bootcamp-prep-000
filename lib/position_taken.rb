e707c094276e520a50c617f2eab24e2cc4b77405
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-13 17:19:49 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, token)
-  if board[token] == " " || board[token] == "" || board[token] == nil
+  if board[token] == " " || board[token] == "" || # board[token] == nil
     false
   end
 end
